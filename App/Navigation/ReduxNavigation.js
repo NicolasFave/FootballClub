c6df6aaa4afa3595be154ddf3b637bba540e4a4e
Olivier THIERRY <olivier.thierry@gmail.com>
2018-05-16 17:58:05 +0200

    Correction bug intégration Redux / React Navigation

--- App/Navigation/ReduxNavigation.js
+++ App/Navigation/ReduxNavigation.js
@@ -1,14 +1,25 @@
 import React from 'react'
 import * as ReactNavigation from 'react-navigation'
+import {
+  createReduxBoundAddListener,
+  createReactNavigationReduxMiddleware,
+} from 'react-navigation-redux-helpers'
 import { connect } from 'react-redux'
 import AppNavigation from './AppNavigation'
 
-// here is our redux-aware smart component
+const middleware = createReactNavigationReduxMiddleware(
+  "root",
+  state => state.nav,
+)
+
+const addListener = createReduxBoundAddListener("root")
+
 function ReduxNavigation (props) {
   const { dispatch, nav } = props
   const navigation = ReactNavigation.addNavigationHelpers({
     dispatch,
-    state: nav
+    state: nav,
+    addListener,
   })
 
   return <AppNavigation navigation={navigation} />
