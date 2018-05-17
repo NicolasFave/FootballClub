006c24dbf8c72d3b39d8f2771e727180ca18714e
Olivier THIERRY <olivier.thierry@gmail.com>
2018-05-17 18:45:48 +0200

    Déclenchement de la saga getCompetitions

--- App/Sagas/index.js
+++ App/Sagas/index.js
@@ -1,5 +1,6 @@
 import { takeLatest, all } from 'redux-saga/effects'
 import API from '../Services/Api'
+import FootballAPI from '../Services/FootballApi'
 import FixtureAPI from '../Services/FixtureApi'
 import DebugConfig from '../Config/DebugConfig'
 
@@ -7,26 +8,32 @@ import DebugConfig from '../Config/DebugConfig'
 
 import { StartupTypes } from '../Redux/StartupRedux'
 import { GithubTypes } from '../Redux/GithubRedux'
+import { ActionTypes as CompetitionsActionTypes } from '../Redux/CompetitionsRedux'
 
 /* ------------- Sagas ------------- */
 
 import { startup } from './StartupSagas'
 import { getUserAvatar } from './GithubSagas'
+import { getCompetitions } from './CompetitionsSagas'
 
 /* ------------- API ------------- */
 
 // The API we use is only used from Sagas, so we create it here and pass along
 // to the sagas which need it.
 const api = DebugConfig.useFixtures ? FixtureAPI : API.create()
+const footballApi = FootballAPI.create()
 
 /* ------------- Connect Types To Sagas ------------- */
 
-export default function * root () {
+export default function* root() {
   yield all([
     // some sagas only receive an action
     takeLatest(StartupTypes.STARTUP, startup),
 
+    takeLatest(CompetitionsActionTypes.COMPETITIONS_REQUEST, getCompetitions, footballApi),
+
     // some sagas receive extra parameters in addition to an action
     takeLatest(GithubTypes.USER_REQUEST, getUserAvatar, api)
+
   ])
 }
