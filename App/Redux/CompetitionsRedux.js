6274fd6b4753aeeabf0e16be49bc5e48927b1643
Olivier THIERRY <olivier.thierry@gmail.com>
2018-05-17 10:24:01 +0200

    Création de CompetitionsRedux + TU

---
+++ App/Redux/CompetitionsRedux.js
@@ -0,0 +1,54 @@
+// Initial state
+
+export const initialState = {
+  collection: []
+}
+
+// Action Types
+
+const SET_COLLECTION = 'SET_COLLECTION'
+
+export const ActionTypes = {
+  SET_COLLECTION,
+}
+
+// Action Creators
+
+const setCollection = (collection) => {
+  return ({
+    type: SET_COLLECTION,
+    payload: collection,
+  })
+}
+
+export const ActionCreators = {
+  setCollection,
+}
+
+// Selectors
+
+const getCollection = (state) => {
+  return state.competitions.collection
+}
+
+export const Selectors = {
+  getCollection,
+}
+
+// Reducer
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_COLLECTION:
+      return {
+        ...state,
+        ...{
+          collection: action.payload,
+        },
+      }
+    default:
+      return initialState
+  }
+}
+
+export default reducer
---
+++ Tests/Redux/CompetitionsRedux.spec.js
@@ -0,0 +1,83 @@
+import reducer, {
+  ActionCreators,
+  ActionTypes,
+  initialState,
+} from '../../App/Redux/CompetitionsRedux'
+
+const competitions = [{
+  key: '447',
+  caption: 'League One',
+}, {
+  key: '448',
+  caption: 'League Two',
+}]
+
+describe('action creators', () => {
+
+  it('should create an action to set collection', () => {
+
+    const expectedAction = {
+      type: ActionTypes.SET_COLLECTION,
+      payload: competitions,
+    }
+
+    expect(ActionCreators.setCollection(competitions)).toEqual(expectedAction)
+
+  })
+
+})
+
+describe('reducer', () => {
+
+  describe('when there is no state', () => {
+
+    it('should return initial state when action type is undefined', () => {
+      expect(reducer(undefined, {})).toEqual(initialState)
+    })
+
+    it('shoud return state with collection', () => {
+
+      const expectedState = {
+        collection: competitions,
+      }
+
+      const action = {
+        type: ActionTypes.SET_COLLECTION,
+        payload: competitions,
+      }
+      expect(reducer(undefined, action)).toEqual(expectedState)
+
+    })
+
+  })
+
+  describe('when there is already a state', () => {
+
+    const state = {
+      collection: [],
+      otherCollection: [],
+    }
+
+    it('should return initial state when action type is undefined', () => {
+      expect(reducer(state, {})).toEqual(initialState)
+    })
+
+    it('shoud return state with competitions', () => {
+
+      const expectedState = {
+        collection: competitions,
+        otherCollection: [],
+      }
+
+      const action = {
+        type: ActionTypes.SET_COLLECTION,
+        payload: competitions,
+      }
+      expect(reducer(state, action)).toEqual(expectedState)
+
+    })
+
+  })
+
+
+})
